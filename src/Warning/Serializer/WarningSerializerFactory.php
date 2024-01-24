ff8b76f939c08a3060d9bbc2b6e6ac59de48933f
Mohit Aghera <mohit.beta@gmail.com>
2024-01-24 16:00:42 +0530

    WIP: Initial commit.

---
+++ src/Warning/Serializer/WarningSerializerFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BomWeather\Warning\Serializer;
+
+use BomWeather\Forecast\Serializer\AreaNormalizer;
+use BomWeather\Forecast\Serializer\ForecastNormalizer;
+use BomWeather\Forecast\Serializer\ForecastPeriodNormalizer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+/**
+ * Factory for creating a weather warning data.
+ */
+class WarningSerializerFactory {
+
+  /**
+   * Creates a new forecast serializer.
+   *
+   * @param string $rootNode
+   *   (optional) The root node of the XML.
+   *
+   * @return \Symfony\Component\Serializer\Serializer
+   *   The serializer.
+   */
+  public static function create(string $rootNode = 'product'): Serializer {
+    $encoders = [new XmlEncoder([XmlEncoder::ROOT_NODE_NAME => $rootNode])];
+    $normalizers = [
+      new DateTimeNormalizer([DateTimeNormalizer::TIMEZONE_KEY => 'UTC']),
+      new WarningNormalizer(),
+      new WarningInfoNormalizer(),
+      new AreaNormalizer(),
+    ];
+    return new Serializer($normalizers, $encoders);
+  }
+
+}
