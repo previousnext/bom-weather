aef086457705b427a928a9b4c61fdb36d3ef838d
Mohit Aghera <mohit.beta@gmail.com>
2026-04-20 03:34:44 +0530

    Add new observation attributes. (#12)
    * Add new attributes.
    
    * phpcs fixes.
    
    * Variable type and return type fixes.
    
    * Set wind direction in degree.
    
    * Adding additional attributes and array access check.
    
    * Change the data type of the pressure value.

--- src/Observation/Wind.php
+++ src/Observation/Wind.php
@@ -35,6 +35,71 @@ final class Wind {
   protected ?int $gustKnots = NULL;
 
   /**
+   * The wind gusts, in kilometres per Hour.
+   */
+  protected ?int $maximumGustKmh = NULL;
+
+  /**
+   * The wind gusts, in knots.
+   */
+  protected ?int $maximumGustSpeed = NULL;
+
+  /**
+   * The wind gusts, in knots.
+   */
+  protected ?string $maximumGustDirection = NULL;
+
+  /**
+   * The wind direction in degree.
+   */
+  protected ?int $windDirectionDegree = NULL;
+
+  /**
+   * Gets the maximum gust speed in kilometers per hour.
+   */
+  public function getMaximumGustKmh(): ?int {
+    return $this->maximumGustKmh;
+  }
+
+  /**
+   * Set the maximum gust speed in kilometers per hour.
+   */
+  public function setMaximumGustKmh(?int $maximumGustKmh): Wind {
+    $this->maximumGustKmh = $maximumGustKmh;
+    return $this;
+  }
+
+  /**
+   * Gets the maximum gust speed in knots.
+   */
+  public function getMaximumGustSpeed(): ?int {
+    return $this->maximumGustSpeed;
+  }
+
+  /**
+   * Sets the maximum gust speed in knots.
+   */
+  public function setMaximumGustSpeed(?int $maximumGustSpeed): Wind {
+    $this->maximumGustSpeed = $maximumGustSpeed;
+    return $this;
+  }
+
+  /**
+   * Sets the maximum gust direction.
+   */
+  public function getMaximumGustDirection(): ?string {
+    return $this->maximumGustDirection;
+  }
+
+  /**
+   * Gets the maximum gust direction.
+   */
+  public function setMaximumGustDirection(string $maximumGustDirection): Wind {
+    $this->maximumGustDirection = $maximumGustDirection;
+    return $this;
+  }
+
+  /**
    * Gets the Direction.
    */
   public function getDirection(): ?string {
@@ -106,6 +171,21 @@ final class Wind {
    */
   public function setGustKnots(int $gustKnots): Wind {
     $this->gustKnots = $gustKnots;
+    return $this;
+  }
+
+  /**
+   * Gets the wind direction in degree.
+   */
+  public function getWindDirectionDegree(): ?int {
+    return $this->windDirectionDegree;
+  }
+
+  /**
+   * Sets the wind direction in degree.
+   */
+  public function setWindDirectionDegree(int $gustKnots): Wind {
+    $this->windDirectionDegree = $gustKnots;
     return $this;
   }
 
